4d77b1a52c9fcc60563002f297d4547be49d63b7
lirsacc <c.lirsac@gmail.com>
2019-11-28 23:38:31 +0000

    Read .env files

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 //! dotenv-run cli
 //! ==============
 //!
-//!
 
 use std::fs;
+use std::io;
 use std::os::unix::process::CommandExt;
-use std::process::{exit, id as get_pid, Command};
+use std::path::PathBuf;
+use std::process::{exit, Command};
 
+use dotenv;
 use structopt::StructOpt;
 use users::{get_group_by_name, get_user_by_name};
 
@@ -28,6 +30,18 @@ struct Options {
   #[structopt(short, long)]
   directory: Option<String>,
 
+  /// .env file(s) to load
+  #[structopt(short = "f", long = "file", multiple = true)]
+  files: Vec<PathBuf>,
+
+  /// Ignore any missing env files
+  #[structopt(long)]
+  ignore_missing: bool,
+
+  /// Disable default .env lookup
+  #[structopt(long)]
+  no_default: bool,
+
   /// Program to execute
   cmd: String,
 
@@ -38,8 +52,6 @@ struct Options {
 fn main() {
   let options = Options::from_args();
 
-  dbg!(get_pid(), &options);
-
   let mut command = Command::new(options.cmd);
 
   command.args(options.args);
@@ -47,6 +59,33 @@ fn main() {
   set_directory(&mut command, options.directory);
   set_user(&mut command, options.user);
   set_group(&mut command, options.group);
+
+  if !options.no_default {
+    match dotenv::dotenv() {
+      Ok(_) => {}
+      Err(e) => {
+        eprintln!("Failed to load default env file: {}", e);
+        exit(1);
+      }
+    }
+  }
+
+  for path in options.files.iter().map(|pb| pb.as_path()) {
+    match dotenv::from_path(path) {
+      Ok(_) => {}
+      Err(dotenv::Error::Io(e)) => {
+        dbg!(&e);
+        if e.kind() != io::ErrorKind::NotFound || !options.ignore_missing {
+          eprintln!("Failed to load env file at {}: {}", path.display(), e);
+          exit(1);
+        }
+      }
+      Err(e) => {
+        eprintln!("Failed to load env file at {}: {}", path.display(), e);
+        exit(1);
+      }
+    }
+  }
 
   // exec doesn't return unless something went wrong.
   let err = command.exec();
